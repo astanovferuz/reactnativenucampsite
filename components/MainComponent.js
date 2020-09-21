987f0d397209823558e483e58b6a33df1c2a964e
Feruz <astanovferuz@gmail.com>
2020-09-21 13:39:36 +0800

    Favorites Exercise

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,6 +7,7 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import Home from "./HomeComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import Favorites from './FavoritesComponent';
 import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from "react-redux";
 import { fetchCampsites, fetchComments, fetchPartners, fetchPromotions } from "../redux/ActionCreators"
@@ -141,6 +142,29 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites },
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -193,6 +217,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
