b31bdc308cfd046f14bb60b1028744a4b6038bc0
Feruz <astanovferuz@gmail.com>
2020-09-22 18:38:02 +0800

    Week 3  All weekly exs finished

--- redux/ConfigureStore.js
+++ redux/ConfigureStore.js
@@ -6,10 +6,18 @@ import { comments } from './commentsReducer';
 import { partners } from "./partnersReducer";
 import { promotions } from "./promotionsReducer";
 import { favorites } from "./favoritesReducer";
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             campsites: campsites,
             partners: partners,
             comments: comments,
@@ -19,5 +27,7 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store);
+
+    return {persistor, store};
 }
