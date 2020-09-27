43aef52ac1853c7c592f7fde4d24716d2046a9a1
Feruz <astanovferuz@gmail.com>
2020-09-27 12:35:08 +0800

    Week 4 - Workshop Assignment Task 1 Completed

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,7 +6,7 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import { createBottomTabNavigator } from "react-navigation"
 import { baseUrl } from "../shared/baseUrl";
-import createStackNavigator from 'react-navigation/src/navigators/createStackNavigator';
+import * as ImageManipulator from "expo-image-manipulator";
 
 class LoginTab extends Component {
 
@@ -155,9 +155,17 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled)   {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri})
+                this.processImage(capturedImage.uri)
             }
         }
+    }
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(imgUri, [{resize: {width: 400}}], {format: ImageManipulator.SaveFormat.PNG});
+        console.log(processedImage);
+        this.setState({
+            imageUrl: processedImage.uri
+        })
     }
 
     handleRegister() {
